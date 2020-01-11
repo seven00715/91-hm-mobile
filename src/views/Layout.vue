6238ec3af593fca47b88f932665a4188c3522916
seven00715 <1027369812@qq.com>
2020-01-11 15:08:24 +0800

    layout布局

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,38 +1,49 @@
 <template>
   <div class="container">
     <!-- 当路由地址（path === $route.path）不是`我的`才显示 -->
-    <van-nav-bar fixed title="黑马头条" right-text="搜索"
-      @click-right="$router.push('/search')"/>
-    <div class="my-wrapper" >
+    <van-nav-bar
+      fixed
+      title="黑马头条"
+      right-text="搜索"
+      @click-right="$router.push('/search')"
+      v-if="showNavBar"
+    />
+    <div class="my-wrapper" :class="{noTop:!showNavBar}">
       <router-view></router-view>
     </div>
-    <van-tabbar route>
+    <van-tabbar route>  <!-- 开启路由模式 -->
       <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item to="/question"  icon="chat-o">问答</van-tabbar-item>
-      <van-tabbar-item to="/video"  icon="video-o">视频</van-tabbar-item>
-      <van-tabbar-item to="/user"  icon="user-o">我的</van-tabbar-item>
+      <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+      <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    showNavBar () {
+      // this 指向vue实例
+      return this.$route.path !== '/user'
+    }
+  }
 }
 </script>
 
 <style scoped lang='less'>
-.container{
+.container {
   width: 100%;
   height: 100%;
   position: relative;
-  .my-wrapper{
+  .my-wrapper {
     width: 100%;
     height: 100%;
     overflow: hidden;
     padding-top: 46px;
     padding-bottom: 50px;
     box-sizing: border-box;
-    &.noTop{
+    &.noTop {
       padding-top: 0;
     }
   }
