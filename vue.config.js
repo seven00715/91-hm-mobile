4a73aa93e321d30cf1c76af2000fc98b158b901a
seven00715 <1027369812@qq.com>
2020-02-13 15:58:34 +0800

    物理返回配置

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,20 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    // 这个config参数就是当前vue-cil项目里的所有的webpack配置信息
+    // 想要改动 直接改这个信息就可以
+
+    // 先判断下当下的环境是否是生产环境
+    if (process.env.NODE_ENV === 'production') {
+      // webpack在生产环境下运行
+      // 把所有的console 都删除 然后再打包
+      // 这里是删除不是删除源代码 ,而是删除打包出来的 console
+      // .drop_console = true 的意思是删除所有的 console
+      //
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  publicPath: './'
 }
